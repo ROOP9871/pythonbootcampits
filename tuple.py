8b132d2bc52ebae914897d60b12f9c7b0866d46c
unknown <roopk8366@gmail.com>
2024-09-17 15:17:43 +0530

    roop

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# Define a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Use a for loop to print each value
+for value in my_tuple:
+    print(value)
+myTuple = ["Roop","Anshu","Deepak","Raman Kumar","Ankita","Veronika","Ritu raj"]
+print(type(myTuple))
+print(myTuple)
+# Use a for loop to print each value of tuple
+for value in myTuple:
+    print(value)
